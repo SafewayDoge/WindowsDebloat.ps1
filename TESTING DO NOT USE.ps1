fc28c5ea94035045af0bf2629db8830a1e58e84f
SafewayDoge <n99230115@gmail.com>
2025-01-25 17:37:24 -0500

    V2 TESTING DO NOT USE.ps1

--- TESTING DO NOT USE.ps1
+++ TESTING DO NOT USE.ps1
@@ -4,13 +4,13 @@ If (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdent
     Exit
 }
 
-# Function to stop any running processes for the app
+# Makes running apps stop lol
 function Stop-AppProcess {
     param ([string]$appName)
     Get-Process -Name $appName -ErrorAction SilentlyContinue | Stop-Process -Force -ErrorAction SilentlyContinue
 }
 
-# Function to remove pre-installed apps
+# removes bloatware
 function Remove-Bloatware {
     $bloatApps = @(
         "Microsoft.YourPhone", "Microsoft.IeCompatApp", "Microsoft.WordPad", "Microsoft.BingWeather",
@@ -21,7 +21,7 @@ function Remove-Bloatware {
         "Microsoft.MicrosoftSolitaireCollection", "Microsoft.WindowsMaps", "microsoft.windowscommunicationsapps", 
         "Microsoft.GrooveMusic", "Microsoft.GetHelp", "Microsoft.FeedbackHub", "Microsoft.Cortana", 
         "Microsoft.Camera", "Microsoft.WindowsAlarms", "Microsoft.3DViewer", "LinkedIn.LinkedIn", 
-        "Microsoft.Todo", "Microsoft.Clipchamp"
+        "Microsoft.Todo", "Microsoft.Clipchamp", "Microsoft.Edge"  # Added Microsoft Edge for removal
     )
 
     foreach ($app in $bloatApps) {
@@ -31,5 +31,17 @@ function Remove-Bloatware {
     }
 }
 
+# Uses WinGet to Install Brave and Remove MS EDGE
+function Install-Brave {
+    Write-Host "Installing Brave Browser..." -ForegroundColor Yellow
+    if (Get-Command winget -ErrorAction SilentlyContinue) {
+        winget install --id=Brave.Brave -e --accept-source-agreements --accept-package-agreements
+        Write-Host "Brave installed successfully!" -ForegroundColor Green
+    } else {
+        Write-Host "Winget is not installed. Please install Winget first." -ForegroundColor Red
+    }
+}
+
 Remove-Bloatware
+Install-Brave
 Restart-Computer -Force
