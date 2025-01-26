8eb39292e4a382388afe31eee9076ba8a6d15f6c
SafewayDoge <n99230115@gmail.com>
2025-01-26 05:28:22 -0500

    v4Windows-Debloat.ps1

--- Debloating/Windows-Debloat.ps1
+++ Debloating/Windows-Debloat.ps1
@@ -1,11 +1,13 @@
-if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]'Administrator')) {
+if (-not ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
     Start-Process powershell -Verb RunAs -ArgumentList "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`""
     exit
 }
+
 $debloatPath = ".\Debloat.txt"
-if (!(Test-Path $debloatPath) -or (Get-Item $debloatPath).LastWriteTime -lt (Get-Date).AddDays(-7)) {
+if (-not (Test-Path $debloatPath) -or (Get-Item $debloatPath).LastWriteTime -lt (Get-Date).AddDays(-7)) {
     Invoke-WebRequest "https://github.com/mrhaydendp/RemoveEdge/raw/main/Individual%20Scripts/Debloat.txt" -OutFile $debloatPath
 }
+
 $disable = Get-Content $debloatPath
 $appxPackages = Get-AppxPackage | Select-Object -ExpandProperty Name
 
