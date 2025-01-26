8dcc0fc799425623231095d308cdf0ba84a0c15b
SafewayDoge <n99230115@gmail.com>
2025-01-26 05:02:40 -0500

    Windows-Debloat.ps1

--- Debloating/Windows-Debloat.ps1
+++ Debloating/Windows-Debloat.ps1
@@ -1,11 +1,6 @@
-# Run with admin privileges if not already
+# RUN AS ADMIN
 if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]'Administrator')) { Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs; exit }
 
-# Update Debloat.txt if older than 7 days
-if ((Get-Item .\Debloat.txt -ErrorAction SilentlyContinue).LastWriteTime -lt (Get-Date).AddDays(-7)) { Invoke-WebRequest "https://github.com/mrhaydendp/RemoveEdge/raw/main/Individual%20Scripts/Debloat.txt" -OutFile Debloat.txt }
+Invoke-WebRequest "https://raw.githubusercontent.com/SafewayDoge/WindowsDebloat.ps1/refs/heads/main/Debloat.txt" -OutFile Debloat.txt
 
-# Remove matching packages
-Get-Content Debloat.txt | ForEach-Object { 
-    $pkg = $_.Split(" #")[0]
-    Get-AppxPackage $pkg | Remove-AppxPackage -ErrorAction SilentlyContinue
-}
+Get-Content Debloat.txt | ForEach-Object { Get-AppxPackage $_.Split(" #")[0] | Remove-AppxPackage -ErrorAction SilentlyContinue }
