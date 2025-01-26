a0b10a41f5e9a1f3a1d7febc4b179fa64ee0a68a
SafewayDoge <n99230115@gmail.com>
2025-01-26 05:25:19 -0500

    v3Windows-Debloat.ps1

--- Debloating/Windows-Debloat.ps1
+++ Debloating/Windows-Debloat.ps1
@@ -1,8 +1,18 @@
-# Run as admin
 if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]'Administrator')) {
     Start-Process powershell -Verb RunAs -ArgumentList "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`""
     exit
 }
+$debloatPath = ".\Debloat.txt"
+if (!(Test-Path $debloatPath) -or (Get-Item $debloatPath).LastWriteTime -lt (Get-Date).AddDays(-7)) {
+    Invoke-WebRequest "https://github.com/mrhaydendp/RemoveEdge/raw/main/Individual%20Scripts/Debloat.txt" -OutFile $debloatPath
+}
+$disable = Get-Content $debloatPath
+$appxPackages = Get-AppxPackage | Select-Object -ExpandProperty Name
 
-Invoke-WebRequest "https://raw.githubusercontent.com/SafewayDoge/WindowsDebloat.ps1/refs/heads/main/Debloat.txt" -OutFile Debloat.txt
-Get-Content Debloat.txt | ForEach-Object { Get-AppxPackage $_.Split(" #")[0] | Remove-AppxPackage -ErrorAction SilentlyContinue }
+foreach ($package in $disable) {
+    $packageName = $package.Split(" #")[0]
+    if ($appxPackages -contains $packageName) {
+        Write-Host "Removing: $packageName"
+        Get-AppxPackage $packageName | Remove-AppxPackage -ErrorAction SilentlyContinue
+    }
+}
