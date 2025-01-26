c3320e740668b631509f9d7833b07ed481d7587b
SafewayDoge <n99230115@gmail.com>
2025-01-26 05:20:12 -0500

    v2Windows-Debloat.ps1

--- Debloating/Windows-Debloat.ps1
+++ Debloating/Windows-Debloat.ps1
@@ -1,6 +1,8 @@
-# RUN AS ADMIN
-if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]'Administrator')) { Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs; exit }
+# Run as admin
+if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]'Administrator')) {
+    Start-Process powershell -Verb RunAs -ArgumentList "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`""
+    exit
+}
 
 Invoke-WebRequest "https://raw.githubusercontent.com/SafewayDoge/WindowsDebloat.ps1/refs/heads/main/Debloat.txt" -OutFile Debloat.txt
-
 Get-Content Debloat.txt | ForEach-Object { Get-AppxPackage $_.Split(" #")[0] | Remove-AppxPackage -ErrorAction SilentlyContinue }
