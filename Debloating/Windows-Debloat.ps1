41337feadb04a71dfb9af131032c1640cf51016c
SafewayDoge <n99230115@gmail.com>
2025-01-26 06:44:59 -0500

    Windows-Debloat.ps1

--- Debloating/Windows-Debloat.ps1
+++ Debloating/Windows-Debloat.ps1
@@ -1,40 +1,26 @@
-# Checks if the script is running as Administrator, if not, re-launch it with elevated privileges
+# Checks if running as Admin, if not, will re-launch as admin
 if (-not ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]'Administrator')) {
     Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs
     exit
 }
-
-# Check if Debloat.txt needs to be updated (older than 7 days)
-$debloatFile = ".\Debloat.txt"
-if ((Get-Item $debloatFile).LastWriteTime -lt (Get-Date).AddDays(-7)){
-    Write-Host "Debloat.txt is outdated. Updating..."
-    Invoke-WebRequest "https://raw.githubusercontent.com/SafewayDoge/WindowsDebloat.ps1/refs/heads/main/Debloating/Debloat.txt" -OutFile $debloatFile
+$disable = @(
+    "Microsoft.3DViewer", "Microsoft.Alarm", "Microsoft.BingWeather", "Microsoft.Camera",
+    "Microsoft.Clipchamp", "Microsoft.Cortana", "Microsoft.FeedbackHub", "Microsoft.GetHelp",
+    "Microsoft.GetStarted", "Microsoft.GrooveMusic", "Microsoft.IeCompatApp", 
+    "Microsoft.MathematicalInputPanel", "Microsoft.MicrosoftSolitaireCollection", 
+    "Microsoft.MixedReality.Portal", "Microsoft.MSPaint", "Microsoft.Office.Desktop", 
+    "Microsoft.Office.OneNote", "Microsoft.OneDrive", "Microsoft.People", "Microsoft.Photos", 
+    "Microsoft.ScreenSketch", "Microsoft.SkypeApp", "Microsoft.StickyNotes", "Microsoft.Todo", 
+    "Microsoft.WindowsAlarms", "Microsoft.WindowsCommunicationsApps", "Microsoft.WindowsMaps", 
+    "Microsoft.WindowsSoundRecorder", "Microsoft.WordPad", "Microsoft.YourPhone", "Microsoft.ZuneVideo", 
+    "Microsoft365Calendar", "Microsoft365Copilot", "InternetExplorer", "LinkedIn"
+)
+$installedApps = Get-AppxPackage | Select-Object -ExpandProperty Name
+$disable | ForEach-Object {
+    if ($installedApps -contains $_) {
+        Write-Host "Removing: $_"
+        try { Get-AppxPackage $_ | Remove-AppxPackage -Verbose } 
+        catch { Write-Warning "Failed to remove $_: $_" }
+    } else { Write-Host "$_ not found. Skipping." }
 }
-
-# Read the list of packages to disable/remove from Debloat.txt
-$disable = Get-Content $debloatFile
-
-# Get all installed AppX packages and save to a file
-(Get-AppxPackage).Name | Out-File appxpackages.txt
-
-# Process each app in the Debloat.txt list
-foreach ($array in $disable) {
-    # Get package name before any comments (splitting by " #")
-    $appName = ($array -split " #")[0]
-    
-    # Check if the package is installed
-    if (Select-String -Quiet $appName appxpackages.txt) {
-        Write-Host "Attempting to remove: $appName"
-        
-        # Attempt to remove the package
-        try {
-            Get-AppxPackage $appName | Remove-AppxPackage -Verbose
-        } catch {
-            Write-Warning "Failed to remove $appName: $_"
-        }
-    } else {
-        Write-Host "$appName not found. Skipping removal."
-    }
-}
-
-Write-Host "Debloating process completed."
+Write-Host "Debloating completed."
