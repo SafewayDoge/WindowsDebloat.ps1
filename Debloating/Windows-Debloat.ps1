db571a7557c54b4adce507dab3e97364baab1dfe
SafewayDoge <n99230115@gmail.com>
2025-01-26 05:50:34 -0500

    v9Windows-Debloat.ps1

--- Debloating/Windows-Debloat.ps1
+++ Debloating/Windows-Debloat.ps1
@@ -2,11 +2,17 @@
 if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]'Administrator')) {
 	Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs; exit
 }
-    Invoke-WebRequest "https://github.com/SafewayDoge/WindowsDebloat.ps1/raw/main/Individual%20Scripts/Debloat.txt" -OutFile Debloat.txt
+
+# If Debloat.txt is older than 7 days, update list
+if ((Get-Item .\Debloat.txt -ErrorAction SilentlyContinue).LastWriteTime -lt (Get-Date).AddDays(-7)){
+    Write-Host "Updating Debloat.txt..."
+    Invoke-WebRequest "https://github.com/mrhaydendp/RemoveEdge/raw/main/Individual%20Scripts/Debloat.txt" -OutFile Debloat.txt
 }
+
 # Read Debloat.txt into $disable variable & grab current list of appxpackages
 $disable = Get-Content Debloat.txt
 (Get-AppxPackage).Name | Out-File appxpackages.txt
+
 # If package from Debloat.txt matches one from appxpackages.txt, attempt to remove it
 foreach ($array in $disable){
     if (Select-String -Quiet $array.split(" #")[0] appxpackages.txt){
@@ -14,4 +20,3 @@ foreach ($array in $disable){
         Get-AppxPackage $array.split(" #")[0] | Remove-AppxPackage -Verbose
     }
 }
-end
